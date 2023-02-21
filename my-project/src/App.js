b810210ac3cb846afdeba9b68fc3e6c9a72f9424
juliain jauregui <julainj9011@gmail.com>
2023-02-21 00:18:55 -0600

    added folders

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,19 @@
-import Nav  from "./components/Nav"
-
+import Navbar  from "./components/Navbar"
+import Header from "./components/Header"
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Button } from 'react-bootstrap';
+
+
+
 function App() {
   return (
+   <>
+   
+    <Navbar />
+    <Header />
     
-    
+    </>
   );
 }
 
---
+++ my-project/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-menu">
+        <li className="nav-item"><a href="#home">Home</a></li>
+        <li className="nav-item"><a href="#about">About</a></li>
+        <li className="nav-item"><a href="#services">Services</a></li>
+        <li className="nav-item"><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
